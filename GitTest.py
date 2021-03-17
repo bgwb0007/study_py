9339b8c2e36aa4e2d433755d1a52907c5875248d
lsh <bgwb0007@naver.com>
2021-03-17 12:24:21 +0900

    bfs_18352

--- GitTest.py
+++ GitTest.py
@@ -1,2 +1,5 @@
 print('Test')
 print('test')
+
+lists=[(2,1),(3,3),(1,2)]
+print(sorted(lists))
---
+++ baekjoon/bfs/18352.특정 거리의 도시 찾기.py
@@ -0,0 +1,29 @@
+## https://www.acmicpc.net/problem/18352
+from collections import deque
+
+N, M, K, X = map(int, input().split())
+lists = [[] for i in range(N+1)]
+for i in range(M):
+    x, y = map(int, input().split())
+    lists[x].append(y)
+dist = [-1 for i in range(N+1)]
+dist[X] = 0
+
+queue = deque()
+queue.append(X)
+while queue:
+    now = queue.popleft()
+
+    for i in lists[now]:
+        if dist[i] == -1:
+            dist[i] = dist[now] + 1
+            queue.append(i)
+
+count=0
+for idx, val in enumerate(dist):
+    if val == K:
+        print(idx)
+        count +=1
+if count ==0:
+    print(-1)
+
