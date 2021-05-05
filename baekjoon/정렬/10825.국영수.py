c310fbe198df65359920e4296d1d4c033b723a5f
lsh <bgwb0007@naver.com>
2021-05-05 19:19:06 +0900

    10825.국영수__정렬_sorted_key 함수

---
+++ baekjoon/정렬/10825.국영수.py
@@ -0,0 +1,17 @@
+## https://www.acmicpc.net/problem/10825
+
+N = int(input())
+
+students = []
+for _ in range(N):
+    temp = input().split()
+    students.append((temp[0], int(temp[1]), int(temp[2]), int(temp[3])))
+
+
+def setting(data):
+    return (-data[1], data[2], -data[3], data[0])
+
+
+answer = sorted(students, key=setting)
+for student in answer:
+    print(student[0])
