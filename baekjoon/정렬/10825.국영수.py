34e542ff81b9d4085a8ef1d8c225ab493932e7ae
lsh <bgwb0007@naver.com>
2021-05-21 12:47:43 +0900

    이진탐색 출력값

--- baekjoon/정렬/10825.국영수.py
+++ baekjoon/정렬/10825.국영수.py
@@ -7,10 +7,8 @@ for _ in range(N):
     temp = input().split()
     students.append((temp[0], int(temp[1]), int(temp[2]), int(temp[3])))
 
-
 def setting(data):
     return (-data[1], data[2], -data[3], data[0])
-
 
 answer = sorted(students, key=setting)
 for student in answer:
--- 프로그래머스/python강의/bisect_이진탐색.py
+++ 프로그래머스/python강의/bisect_이진탐색.py
@@ -3,10 +3,14 @@
 import bisect
 mylist = [1, 2, 3, 3, 7, 9, 11, 33]
 print(bisect.bisect(mylist, 3))
+### >> 4
 
-# 중복되는거 있으면 오른쪽거 탐색
+# 중복되는거 있으면 오른쪽 들어갈 자리 탐색
 print(bisect.bisect_right(mylist,3))
-# 왼쪽거 탐색
+### >> 4
+
+# 왼쪽 들어갈자리 탐색
 print(bisect.bisect_left(mylist,3))
+### >> 2
 
 print(float('inf'))
